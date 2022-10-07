87e24a99ef3d7b7044974c81be599c99cf401484
nopenah <@>
2022-10-06 23:53:05 -0400

    moved all content of .h files for part 1 into map.h

--- Map/Header/Map.h
+++ Map/Header/Map.h
@@ -1,14 +1,15 @@
 #pragma once
-
+#include <vector>
+#include <string>
+#include <ostream>
 #include <list>
-
-#include "Continent.h"
 using namespace std;
+
+Continent* getContinent(std::list<Continent*> Li, string name);
+Territory* getTerritory(std::list<Territory*> Li, string name);
+
 class Map
 {
-	
-
-
 public:
 	list<Continent*> m_Continents; // list of all the continents
 	std::list<Territory*> m_Territories;
@@ -24,7 +25,55 @@ public:
 
 	Map(const Map& ter);
 	Map& operator=(const Map& map);
+};
 
+class Continent
+{
+public:
+	string* m_name;
+	std::list<Territory*> m_Territories; // store all territories in this continent 
+	std::list<Continent*> m_Connections; // store the continent it is connected to 
 
+	Continent(string line);
+	~Continent();
+	bool addTerritory(Territory* ter);
 
+	bool AddConnection(Continent* con);
+	friend ostream& operator<<(ostream& strm, const Continent& cont);
+	Continent(const Continent& ter);
+	Continent& operator=(const Continent& ter);
+};
+
+class Territory
+{
+public:
+	string* m_name;
+	string* m_x;
+	string* m_y;
+	string* m_continent_name;
+	list<Territory*> m_Connections; // store the continent it is connected to 
+	~Territory();
+	Territory(const Territory& ter);
+	Territory& operator=(const Territory& ter);
+	Territory(std::string line);
+	bool AddConnection(Territory* ter);
+	friend ostream& operator<<(ostream& strm, const Territory& cont);
+};
+
+class MapLoader
+{
+public:
+	MapLoader();
+	~MapLoader();
+
+	void Print(std::list<Continent*> Li);
+	void Print(std::list<Territory*> Li);
+	std::string* m_map_desc;
+	Map* m_map;
+	void LoadMap();
+	void Link(string Line);
+	void ReadDescription(std::string FilePath);
+	MapLoader& operator=(const MapLoader& map);
+	MapLoader(const MapLoader& map);
+	friend ostream& operator<<(ostream& strm, const MapLoader& map);
 };
--- Map/Header/Utility_map.h
+++ Map/Header/Utility_map.h
@@ -1,6 +1,6 @@
-#include "Continent.h"
-#include "Territory.h"
-
-
-Continent* getContinent(std::list<Continent*> Li, string name);
-Territory* getTerritory(std::list<Territory*> Li, string name);
+//#include "Continent.h"
+//#include "Territory.h"
+//
+//
+//Continent* getContinent(std::list<Continent*> Li, string name);
+//Territory* getTerritory(std::list<Territory*> Li, string name);
