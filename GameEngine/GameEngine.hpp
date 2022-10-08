025da920c65ebd70fc28dea6e650b57439c08c17
nopenah <@>
2022-10-08 19:56:11 -0400

    comment out entire game engine for now

--- GameEngine/GameEngine.hpp
+++ GameEngine/GameEngine.hpp
@@ -1,47 +1,47 @@
-#pragma once
-#include <iostream>
-#include <string>
-#include <stdio.h>
-#include "Map.h"
-#include "Cards.h"
-#include "Orders.h"
-#include "Player.hpp"
-#include "MapLoader.h"
-using std::cout;
-using std ::string;
-using namespace std;
-
-class GameEngine{
-private:
-    string state;
-    void loadMap();
-    void gameStart();
-    void setState(string);
-public:
-    GameEngine();
-    GameEngine(string);
-    ~GameEngine();
-    string getState();
-    GameEngine(const GameEngine &g1);
-    void transition(string);
-    void readCommand(string);
-    void testGameStates();
-    
-};
-class Command{
-private:
-   // std::Command;
-public:
-  //  setCommand(std::string);
-};
-class CommandProcessor {
-private:
-   // std::cin<<std::pair<std::string, std::pair<std::string, std::string>>> valid_commands_and_their_transitions;
-   // std::cin<<std::string> possible_valid_commands;
-   // std::cin<<Command*> commands;
-   // std::cin<<Command*> readCommand();
-public:
-    CommandProcessor();
-    void getCommand(std::string);
-};
-
+//#pragma once
+//#include <iostream>
+//#include <string>
+//#include <stdio.h>
+//#include "Map.h"
+//#include "Cards.h"
+//#include "Orders.h"
+//#include "Player.hpp"
+//#include "MapLoader.h"
+//using std::cout;
+//using std ::string;
+//using namespace std;
+//
+//class GameEngine{
+//private:
+//    string state;
+//    void loadMap();
+//    void gameStart();
+//    void setState(string);
+//public:
+//    GameEngine();
+//    GameEngine(string);
+//    ~GameEngine();
+//    string getState();
+//    GameEngine(const GameEngine &g1);
+//    void transition(string);
+//    void readCommand(string);
+//    void testGameStates();
+//    
+//};
+//class Command{
+//private:
+//   // std::Command;
+//public:
+//  //  setCommand(std::string);
+//};
+//class CommandProcessor {
+//private:
+//   // std::cin<<std::pair<std::string, std::pair<std::string, std::string>>> valid_commands_and_their_transitions;
+//   // std::cin<<std::string> possible_valid_commands;
+//   // std::cin<<Command*> commands;
+//   // std::cin<<Command*> readCommand();
+//public:
+//    CommandProcessor();
+//    void getCommand(std::string);
+//};
+//
--- GameEngine/GameEngineDriver.cpp
+++ GameEngine/GameEngineDriver.cpp
@@ -5,19 +5,19 @@
 using namespace std;
 
      
-void testGameStates(){
-
-    setState(start);
-    const char* states[8]={"start","map loaded","map validated","players added","assign reinforcement","issue orders","execute orders","win"};
-        cout<<"enter your command"<<endl;
-        while(true){
-            cin>>tempStr;
-            if(tempStr=="win"){
-                break;
-                
-            }
-            if(tempStr=="loadmap"){
-                cout << "map loaded";
-            }
-        
-         }
+//void testGameStates(){
+//
+//    setState(start);
+//    const char* states[8]={"start","map loaded","map validated","players added","assign reinforcement","issue orders","execute orders","win"};
+//        cout<<"enter your command"<<endl;
+//        while(true){
+//            cin>>tempStr;
+//            if(tempStr=="win"){
+//                break;
+//                
+//            }
+//            if(tempStr=="loadmap"){
+//                cout << "map loaded";
+//            }
+//        
+//         }
