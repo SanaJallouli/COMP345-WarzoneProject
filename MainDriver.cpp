0f0e0eb0677f5d5c40ab673888216601316ffce1
nopenah <@>
2022-10-08 22:48:12 -0400

    added testGameStates() to MainDriver.cpp

--- MainDriver.cpp
+++ MainDriver.cpp
@@ -1,23 +1,33 @@
 #include "Map_driver.h"
-#include "CardsDriver.h"
+#include "GameEngine.h"
+#include "Orders.h"
+#include "Cards.h"
 #include <string>
 #include <iostream>
 
 
 int main(){
 
-	//map part 1
+	//part 1: map
 	std::cout << std::endl 
 		<< " ===== testLoadMaps() start ===== " << std::endl;
 	//testLoadMaps();
 	std::cout << std::endl
 		<< " ===== testLoadMaps() end ===== " << std::endl;
 
+	//part 2: player
+
+	//part 3: order
+	//testOrdersList();
+
 	//card part 4
 	testCards(); 
 
+	//part 5: game engine
+	testGameStates();
+
 	//pause the console
-	std::cout << std::endl << "Press any key and enter to exit: ";
+	std::cout << std::endl << "Press any key then enter to exit: ";
 	std::string input;
 	std::cin >> input;
 
