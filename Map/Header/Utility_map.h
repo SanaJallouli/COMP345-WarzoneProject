a492195f6500c5566da2e2b6ee7364669f60e969
nopenah <@>
2022-10-08 21:41:38 -0400

    deleted old files

--- Map/Header/Utility_map.h
+++
@@ -1,6 +0,0 @@
-//#include "Continent.h"
-//#include "Territory.h"
-//
-//
-//Continent* getContinent(std::list<Continent*> Li, string name);
-//Territory* getTerritory(std::list<Territory*> Li, string name);
